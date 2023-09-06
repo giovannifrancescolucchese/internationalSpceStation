b3a3a5d1938e8fc2326768b89ebb6dc975eeb26d
defilii <alessiodefilippimg@gmail.com>
2023-09-06 08:53:05 +0200

    test commit

---
+++ src/test/java/org/example/converter/FluidConverterTest.java
@@ -0,0 +1,16 @@
+package org.example.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FluidConverterTest {
+
+    @Test
+    void setGalloni() {
+    }
+
+    @Test
+    void setLitri() {
+    }
+}
